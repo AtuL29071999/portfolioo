f3105409cabdec1606ee26b3715516b10294cb67
AtuL29071999 <atulkr803@gmail.com>
2024-01-13 00:40:01 +0530

    almost done

---
+++ src/content_option.js
@@ -0,0 +1,183 @@
+const logotext = "Atul Kumar";
+const meta = {
+  title: "Atul Kumar",
+  description: "I’m Atul Kumar Java Full stack devloper",
+};
+
+const introdata = {
+  title: "I’m Atul Kumar",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop Web Applications.",
+  },
+  description:
+    "As a Java Full Stack Developer, I adeptly navigate both front-end and back-end technologies, crafting robust server-side solutions with Java, Spring, and Spring Boot. On the front-end, I create dynamic interfaces using HTML, CSS, and JavaScript, often employing frameworks like React. Proficient in SQL and MySQL databases, I ensure efficient data management. Collaborative and detail-oriented, I contribute across the software development lifecycle, delivering high-quality, scalable solutions. Committed to staying current with industry trends, my passion for problem-solving drives the innovation and reliability essential for the success of web-based projects.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "Passionate Java Full Stack Developer Building Innovative Web Solutions",
+  aboutme:
+    "Hello! I'm Atul Kumar, a seasoned Java Full Stack Developer passionate about building robust and scalable web applications. With a solid foundation in Java, Spring, and Spring Boot, I bring 1 Month of expertise in crafting end-to-end solutions. Proficient in both front-end technologies like HTML, CSS, and JavaScript React, and back-end development with RESTful APIs, databases SQL/MySQL, and server management. My commitment to clean code and innovative problem-solving ensures the delivery of high-quality software. Let's connect and explore the possibilities of creating exceptional digital experiences.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Java full Stack developer",
+    where: "Atulya Tech PVT LTD.",
+    date: "2023",
+  },
+  {
+    jobtitle: "Getting Tranning from Jspider",
+    where: "Noida",
+    date: "2023",
+  },
+  {
+    jobtitle: "Student Of MCA",
+    where: "Noida",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "Java",
+    value: 90,
+  },
+  {
+    name: "J2EE",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Tailwind",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Welcome to my UI & UX Design world! I am Atul Kumar, a creative mind dedicated to crafting visually stunning and intuitively functional digital experiences. With a meticulous approach to user interface design, I bring concepts to life, ensuring seamless navigation and engaging aesthetics. My UX design expertise focuses on understanding user behaviors, wireframing, and prototyping to create interfaces that not only look beautiful but also enhance user satisfaction. Let's embark on a journey to transform ideas into captivating and user-centric designs that leave a lasting impact.",
+  },
+  {
+    title: "Spring & Spring Boot",
+    description:
+      "Hi there! I'm Atul Kumar, and I love building things with Java. I specialize in using Spring and Spring Boot to create powerful and efficient applications. With Spring, I make sure the code is organized and easy to maintain, and with Spring Boot, I can quickly bring ideas to life. Let's chat about how we can use these tools to make your projects awesome!",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "atulkr803gmail.com",
+  YOUR_PHONE: "+91-9142648162",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
